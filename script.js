13a156d136f3b152d140f36ac69adf06b07e47de
Moarari <matko.2010.matko@gmail.com>
2026-03-29 18:01:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,26 @@
 const mojDivVJs = document.getElementById("mojDivVJs")
 
 fetch("students.json")
-    .then((odpoved_raw) => odpoved_raw.json())
+    .then((resp) => resp.json())
     .then((students) => {
+        console.log("Načítané dáta:", students)
 
         students.forEach((student) => {
-
-            
             const card = document.createElement("div")
-            card.style.display = "inline-block"
-            card.style.margin = "10px"
-            card.style.textAlign = "center"
-            card.style.fontFamily = "Arial"
-            
-           
+            card.className = "card"
+
             const img = document.createElement("img")
             img.src = student.image
-            img.width = 200
             img.alt = student.name
-            img.style.borderRadius = "10px"
-            img.style.boxShadow = "0 2px 6px rgba(0,0,0,0.2)"
 
-            
             const name = document.createElement("p")
             name.textContent = student.name
-            name.style.marginTop = "8px"
-            name.style.fontWeight = "bold"
 
-          
             card.appendChild(img)
             card.appendChild(name)
-
-        
             mojDivVJs.appendChild(card)
         })
     })
-    .catch((chyba) => {
-        console.error("Nastala chyba pri fetch:", chyba)
+    .catch((err) => {
+        console.error("Nastala chyba pri fetch:", err)
     })
